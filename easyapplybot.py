37a9b2a9fa7921f6d1532393e1e9cf0cb767f4c4
Adithya <adithya22@protonmail.com>
2023-09-17 17:10:45 -0400

    Add back one import whichwas wrongly removed

--- easyapplybot.py
+++ easyapplybot.py
@@ -7,6 +7,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait  # type: ignore
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.chrome import ChromeDriverManager
 
 import pandas as pd
 from datetime import datetime, timedelta
